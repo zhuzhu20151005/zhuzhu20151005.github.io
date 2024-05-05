2998dccc416e08122578ecbd316827c190b29e5d
zhuzhu <596870757@qq.com>
2024-05-05 12:21:54 +0800

    Site updated: 2024-05-05 12:21:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/03/hello-world/","2024/05/04/运动打卡/"];function toRandomPost(){
+var posts=["2024/05/03/博客首页/","2024/05/04/运动打卡/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
